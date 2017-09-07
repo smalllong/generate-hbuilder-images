7399dc34d796c08fe7b84913d1f2747a7755e5ed
Smalllong <898310778@qq.com>
2017-09-07 14:43:12 +0800

    Create gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp')
+var imageResize = require('gulp-image-resize')
+var rename = require('gulp-rename')
+
+
+gulp.task('icon-ios', () => {
+	var sizes = [29, 40, 50, 57, 58, 72, 76, 80, 87, 100, 114, 120, 144, 152, 180]
+	sizes.forEach((size, index) => {
+		gulp.src('icon.png')
+			.pipe(imageResize({
+				width: size,
+				height: size,
+				crop: true,
+				cover: true,
+			}))
+			.pipe(rename('icon'+size+'.png'))
+			.pipe(gulp.dest('dist'))
+	})
+})
+
+gulp.task('splash-ios', () => {
+	var sizes = [[640, 1136, 568, 2], [750, 1334, 667, 2], [1242, 2208, 736, 3]];
+	sizes.forEach((size, index) => {
+		gulp.src('splash.png')
+			.pipe(imageResize({
+				width: size[0],
+				height: size[1],
+				crop: true,
+				cover: true,
+				upscale: true,
+			}))
+			.pipe(rename('Default-'+size[2]+'h@'+size[3]+'x.png'))
+			.pipe(gulp.dest('dist'))
+	})
+})
